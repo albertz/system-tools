50b1cc97e0d135088af5d65a3cb0ac4ab76a72b2
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2014-07-22 12:39:26 +0200

    more

--- lib/utils.py
+++ lib/utils.py
@@ -46,8 +46,40 @@ def sysexec(*args):
 	import subprocess
 	res = subprocess.call(args, shell=False)
 	if res != 0: raise ShellError
-	
-	
+
+def sysexecOut(*args):
+	from subprocess import Popen, PIPE
+	p = Popen(args, shell=False, stdin=PIPE, stdout=PIPE)
+	out, _ = p.communicate()
+	if p.returncode != 0: raise ShellError
+	out = out.decode("utf-8")
+	return out
+
+def sysexecRetCode(*args, valid=(0,1)):
+	import subprocess
+	res = subprocess.call(args, shell=False)
+	if valid is not None:
+		if res not in valid: raise ShellError
+	return res
+
+
+def git_topLevelDir():
+	d = sysexecOut("git", "rev-parse", "--show-toplevel").strip()
+	test(os.path.isdir(d))
+	test(os.path.isdir(d + "/.git"))
+	return d
+
+def git_headCommit():
+	return sysexecOut("git", "rev-parse", "--short", "HEAD").strip()
+
+def git_isDirty():
+	r = sysexecRetCode("git", "diff", "--no-ext-diff", "--quiet", "--exit-code")
+	if r == 0: return False
+	if r == 1: return True
+	test(False)
+
+
+
 def test_server(servername):
 	import subprocess
 	res = subprocess.call("ping -c 1 %s >/dev/null" % servername, shell=True)
