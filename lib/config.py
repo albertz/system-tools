3ba536f5c1ac0180f7bca775eb4a7077cc696c4a
Albert Zeyer <albzey@gmail.com>
2017-12-30 13:08:47 +0100

    cleanup, tab to spaces, etc

--- lib/config.py
+++ lib/config.py
@@ -1,50 +1,57 @@
 
+import os
 from .utils import CachedFunc0Deco
 
+
 class Config:
-	def __init__(self, filename):
-		self.filename = filename
-		self.data = {}
-		self.load()
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = {}
+        self.load()
 
-	def load(self):
-		try:
-			self.data = eval(open(self.filename).read())
-			assert isinstance(self.data, dict)
-		except Exception:
-			self.data = {}
+    def load(self):
+        try:
+            self.data = eval(open(self.filename).read())
+            assert isinstance(self.data, dict)
+        except Exception:
+            self.data = {}
 
-	def write(self):
-		f = open(self.filename, "w")
-		f.write("dict([\n")
-		f.writelines([repr(l) + ",\n" for l in sorted(self.data.items())])
-		f.write("])\n")
+    def write(self):
+        f = open(self.filename, "w")
+        f.write("dict([\n")
+        f.writelines([repr(l) + ",\n" for l in sorted(self.data.items())])
+        f.write("])\n")
 
-	def get(self, attr, testFunc):
-		if not attr in self.data: self.load() # try to reload
-		if not attr in self.data:
-			import ui
-			res = ui.userInput("Enter %s: " % attr, testFunc=testFunc)
-			self.data[attr] = res
-			self.write()
-			return res
-		res = self.data[attr]
-		# TODO: maybe run testFunc on it and if it fails, reask or so
-		return res
+    def get(self, attr, testFunc):
+        if attr not in self.data:
+            self.load() # try to reload
+        if attr not in self.data:
+            from .ui import userInput
+            res = userInput("Enter %s: " % attr, testFunc=testFunc)
+            self.data[attr] = res
+            self.write()
+            return res
+        res = self.data[attr]
+        # TODO: maybe run testFunc on it and if it fails, reask or so
+        return res
 
-import os
+
 config = Config(filename=os.path.expanduser("~/.system-tools.config"))
+
 
 @CachedFunc0Deco
 def get_private_server():
-	from utils import test_server
-	return config.get("private_server", testFunc=test_server)
+    from .utils import test_server
+    return config.get("private_server", testFunc=test_server)
+
 
 @CachedFunc0Deco
 def get_private_remote_projectdir():
-	from utils import test_remotedir
-	def test(p): return test_remotedir("ssh://%s/%s" % (get_private_server(), p))
-	return config.get("private_remote_projectdir", testFunc=test)
+    from .utils import test_remotedir
+
+    def test(p):
+        return test_remotedir("ssh://%s/%s" % (get_private_server(), p))
+    return config.get("private_remote_projectdir", testFunc=test)
 
 
 
--- lib/ui.py
+++ lib/ui.py
@@ -4,96 +4,111 @@
 import sys
 
 try:
-	import readline
+    import readline
 except ImportError:
-	readline = None
+    # noinspection SpellCheckingInspection
+    readline = None
 if readline:
-	readline.parse_and_bind("tab: complete")
-	readline.parse_and_bind("set show-all-if-ambiguous on")
+    readline.parse_and_bind("tab: complete")
+    readline.parse_and_bind("set show-all-if-ambiguous on")
+
 
 # Python 3 workaround
-try: raw_input
-except NameError: raw_input = input
-
-
+try:
+    # noinspection PyUnresolvedReferences,PyUnboundLocalVariable
+    raw_input
+except NameError:
+    raw_input = input
 
 
 class Completer:
-	def __init__(self, words):
-		self.words = words
-		self.prefix = None
+    def __init__(self, words):
+        self.words = words
+        self.prefix = None
 
-	def complete(self, prefix, index):
-		if prefix != self.prefix:
-			self.matching_words = [w for w in self.words if w.startswith(prefix)]
-			self.prefix = prefix
-		else:
-			pass
-		try:
-			return self.matching_words[index]
-		except IndexError:
-			return None
+    def complete(self, prefix, index):
+        if prefix != self.prefix:
+            self.matching_words = [w for w in self.words if w.startswith(prefix)]
+            self.prefix = prefix
+        else:
+            pass
+        try:
+            return self.matching_words[index]
+        except IndexError:
+            return None
 
 
 WordsReprMaxLen = 40
 
+
 def userInputByChoice(words, prompt=""):
-	if readline:
-		readline.set_completer(Completer(words).complete)
-	words_repr = ", ".join(words)
-	if len(words_repr) > WordsReprMaxLen:
-		words_repr = words_repr[:WordsReprMaxLen] + "..."
-	while True:
-		s = raw_input(prompt + "Choice of [%s] ? " % words_repr)
-		if s in words: return s
-		print("Error: %r unknown, type again" % s)
+    if readline:
+        readline.set_completer(Completer(words).complete)
+    words_repr = ", ".join(words)
+    if len(words_repr) > WordsReprMaxLen:
+        words_repr = words_repr[:WordsReprMaxLen] + "..."
+    while True:
+        s = raw_input(prompt + "Choice of [%s] ? " % words_repr)
+        if s in words:
+            return s
+        print("Error: %r unknown, type again" % s)
+
 
 def confirm(question):
-	while True:
-		s = raw_input("%s Press enter to confirm or Ctrl+C otherwise." % question)
-		if s == "": return
-		print("Error: Don't type anything, just press enter.")
+    while True:
+        s = raw_input("%s Press enter to confirm or Ctrl+C otherwise." % question)
+        if s == "":
+            return
+        print("Error: Don't type anything, just press enter.")
+
 
 def seriousConfirm(question):
-	while True:
-		s = raw_input("%s Type 'yes' to confirm." % question)
-		if s == "yes": return
-		if not s: continue
-		print("Invalid answer %r. Aborting." % s)
-		sys.exit(1)
+    while True:
+        s = raw_input("%s Type 'yes' to confirm." % question)
+        if s == "yes":
+            return
+        if not s:
+            continue
+        print("Invalid answer %r. Aborting." % s)
+        sys.exit(1)
+
 
 def userInput(prompt, testFunc):
-	while True:
-		s = raw_input(prompt)
-		try:
-			assert testFunc(s)
-		except Exception as e:
-			print("Error: %s" % e)
-		else:
-			return s
+    while True:
+        s = raw_input(prompt)
+        try:
+            assert testFunc(s)
+        except Exception as e:
+            print("Error: %s" % e)
+        else:
+            return s
+
 
 # This is here in `ui` because it returns always some string
 # which is only intended for a user and does not mean anything
 # otherwise.
 def getFuncName(func):
-	if hasattr(func, "func_name"):
-		return func.func_name
-	if hasattr(func, "__name__"):
-		return func.__name__
-	return str(func)
+    if hasattr(func, "func_name"):
+        return func.func_name
+    if hasattr(func, "__name__"):
+        return func.__name__
+    return str(func)
+
 
 def userRepr(arg):
-	import inspect
-	if inspect.isroutine(arg):
-		return getFuncName(arg)
-	return repr(arg)
+    import inspect
+    if inspect.isroutine(arg):
+        return getFuncName(arg)
+    return repr(arg)
+
 
 class ConfirmByUserDeco(object):
-	def __init__(self, func):
-		self.func = func
-	def __call__(self, *args, **kwargs):
-		confirm("%s(%s)?" % (
-			getFuncName(self.func),
-			", ".join(list(map(userRepr, args)) + ["%s=%r" % item for item in kwargs.items()])
-		))
-		return self.func(*args, **kwargs)
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        confirm("%s(%s)?" % (
+            getFuncName(self.func),
+            ", ".join(list(map(userRepr, args)) + ["%s=%r" % item for item in kwargs.items()])
+        ))
+        return self.func(*args, **kwargs)
